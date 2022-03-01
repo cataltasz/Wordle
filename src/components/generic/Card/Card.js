422f07221e2e514d72ccea11cb2c0efa9764d2bb
cataltasz <mustafacataltas@yandex.com>
2022-03-01 11:40:30 +0300

    dev

--- src/components/generic/Card/Card.js
+++ src/components/generic/Card/Card.js
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./Card.scss";
 export default function Card({ cardInfo, cardClass }) {
+  const [imgLoaded, setImgLoaded] = useState(false);
   return (
     <Link to={cardInfo.url}>
       <div className={"Card " + cardClass}>
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            {cardInfo.img === null ? (
+            {(cardInfo.img === null || !imgLoaded) && (
               <div className="card-info-placeholder">
                 <span className="card-text">
                   <b>{cardInfo.name} </b>
                 </span>
                 <span className="card-text"> {cardInfo.author} </span>
               </div>
-            ) : (
-              <img src={cardInfo.img} alt="cover" className="card-img" />
+            )}
+
+            {cardInfo.img !== null && (
+              <img
+                src={cardInfo.img}
+                alt="cover"
+                className="card-img"
+                onLoad={() => setImgLoaded(true)}
+              />
             )}
           </div>
           <div className="card-info">
